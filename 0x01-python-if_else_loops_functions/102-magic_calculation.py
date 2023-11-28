2e7a8733c47c380c2ab07b60dc79e0a9f902a39a
Starford5 <ikechukwustarford@gmail.com>
2023-11-28 02:47:42 -0800

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author - Ogbonna ikechukwu
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
+
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author Ogbonna Ikechukwu
+
+def pow(a, b):
+    return (a ** b)
+
